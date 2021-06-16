1919994773b9252565942e715d3ec583732af118
carlosjavierjimenezvasquez <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2021-06-16 11:11:02 -0600

    SP_detalle

--- Script SQL/Stored procedure/CRUDS/Sp_DetalleVenta.sql
+++ Script SQL/Stored procedure/CRUDS/Sp_DetalleVenta.sql
@@ -11,6 +11,7 @@ create PROCEDURE sp_InsertDetalleVenta
 		Insert into DetalleVenta(Venta_FK,Producto_FK,CantidadVProducto,TotalVenta) values (@Venta_Fk,@Producto_fk,@CantidadVProducto,@TotalVenta)
 	END
 	GO
+--****************************************************************************************************
 
 CREATE PROCEDURE sp_ConsultarDetalleVenta
 		
@@ -20,3 +21,45 @@ CREATE PROCEDURE sp_ConsultarDetalleVenta
 		Select * from  DetalleVenta
 	END
 	GO
+
+--****************************************************************************************************
+
+CREATE PROCEDURE sp_ConsultarDetalleVentaId
+		@ID int
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+		Select * from  DetalleVenta where  Id_DetalleVenta = @ID
+	END
+	GO
+	
+
+
+--*********************************************************************************************************************
+CREATE PROCEDURE sp_DeleteDetalleVenta
+		
+		@Id_DetalleVenta  int
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+		Delete from  DetalleVenta where  Id_DetalleVenta=@Id_DetalleVenta
+	END
+	GO
+
+--***********************************************************************************
+CREATE PROCEDURE sp_UpdateDetalleVenta
+		
+		@Venta_Fk int,
+		@IdDetalleVenta  int,
+		@Producto_Fk int,
+		@CantidadVProducto int,
+		@TotalVenta int
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+		Update DetalleVenta set Venta_FK=@Venta_Fk,@Producto_Fk =@Producto_Fk,
+		CantidadVProducto=@CantidadVProducto,TotalVenta=@TotalVenta where Id_DetalleVenta=@IdDetalleVenta
+	END
+	GO
+
+--**************************************************************************************
